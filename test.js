a10c1f6f41a84255acd6570f5950189111320083
jh-210 <114121914+OvertheHandDeveloper@users.noreply.github.com>
2023-09-15 09:16:58 +0900

    .

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var P2Steps=0;
 var P2Steps_=0;
 
 var P4Image = 0;
+var P5Image = 0;
 
 function ClickBtn(x)
 {
@@ -96,11 +97,18 @@ function BacktoP3()
     location.replace('Page3.html');
 }
 
+function SetP4_Img()
+{
+    var urlParameter = window.location.search;
+    var param = new URLSearchParams(urlParameter).get('P4Image');
+    var num = Number(param);
+    ImageSwitch(num);
+}
+
 function GotoP4_Img()
 {
-    console.log(P4Image);
-    location.replace('Page4_1.html');
-    ImageSwitch(0);
+    location.replace('Page4_1.html?P4Image='+P4Image);
+    setTimeout(ImageSwitch(0), 300);
 }
 
 function ImageSwitch(num)
@@ -172,11 +180,105 @@ function ImageSwitch(num)
             if(Img3_!=null) Img3_.style.visibility='visible';
         }
     }
+
+    console.log(P4Image);
 }
 
 function BacktoP4()
 {
-    location.replace('Page4.html');
+    location.replace('Page4.html?P4Image='+P4Image);
+}
+
+function ClickInfo(info)
+{
+    var info_ = document.getElementById(info);
+
+    if(info_ != null)
+    {
+        if(info_.style.visibility == 'hidden')
+        {
+            info_.style.visibility = 'visible';
+        }
+        else info_.style.visibility = 'hidden';
+    }
+}
+
+function ImageSwitch_P5(num)
+{
+    P5Image = P5Image+num;
+
+    var btn_back_off = document.getElementById('back_off');
+    var btn_back_on = document.getElementById('back_on');
+    var btn_go_off = document.getElementById('go_off');
+    var btn_go_on = document.getElementById('go_on');
+
+    var Img1 = document.getElementById('P5_Img1');
+    var Img2 = document.getElementById('P5_Img2');
+    var Img3 = document.getElementById('P5_Img3');
+
+    var Img1_ = document.getElementById('P5_Img1_');
+    var Img2_ = document.getElementById('P5_Img2_');
+    var Img3_ = document.getElementById('P5_Img3_');
+
+    Img1.style.visibility='hidden';
+    Img2.style.visibility='hidden';
+    Img3.style.visibility='hidden';
+
+    if(Img1_ != null)
+    {
+        Img1_.style.visibility='hidden';
+        Img2_.style.visibility='hidden';
+        Img3_.style.visibility='hidden';
+    }
+
+    if(P5Image==0)
+    {
+        btn_back_off.style.visibility='visible';
+        btn_back_on.style.visibility='hidden';
+        btn_go_off.style.visibility='hidden';
+        btn_go_on.style.visibility='visible';
+        
+        Img1.style.visibility='visible';
+        if(Img1_!=null) Img1_.style.visibility='visible';
+    }
+    else if(P5Image == 2)
+    {
+        btn_back_off.style.visibility='hidden';
+        btn_back_on.style.visibility='visible';
+        btn_go_off.style.visibility='visible';
+        btn_go_on.style.visibility='hidden';
+
+        Img3.style.visibility='visible';
+        if(Img3_!=null) Img3_.style.visibility='visible';
+    }
+    else
+    {
+        btn_back_off.style.visibility='hidden';
+        btn_back_on.style.visibility='visible';
+        btn_go_off.style.visibility='hidden';
+        btn_go_on.style.visibility='visible';
+        Img2.style.visibility='visible';
+        if(Img2_!=null) Img2_.style.visibility='visible';
+    }
+}
+
+function GotoP5_Img(num)
+{
+    location.replace('Page5_1.html?P5Image='+num);
+    setTimeout(ImageSwitch(0), 300);
+}
+
+function SetP5_Img()
+{
+    var urlParameter = window.location.search;
+    var param = new URLSearchParams(urlParameter).get('P5Image');
+    var num = Number(param);
+    ImageSwitch_P5(num);
+}
+
+function BacktoP5()
+{
+    location.replace('Page5.html');
 }
 
 function test()
